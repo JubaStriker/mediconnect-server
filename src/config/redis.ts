b186b7cac546c85413537029a7b2410e65e964c7
JubaStriker <jubastriker@gmail.com>
2025-10-20 02:06:14 +0600

    Redis connection established

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,7 +1,17 @@
 import { createClient } from 'redis';
 
+const REDIS_USERNAME = process.env.REDIS_USERNAME || "default";
+const REDIS_PASSWORD = process.env.REDIS_PASSWORD;
+const REDIS_HOST = process.env.REDIS_HOST;
+const REDIS_PORT = Number(process.env.REDIS_PORT) || 6379;
+
 const redisClient = createClient({
-    url: process.env.REDIS_URL || 'redis://localhost:6379'
+    username: REDIS_USERNAME,
+    password: REDIS_PASSWORD,
+    socket: {
+        host: REDIS_HOST,
+        port: REDIS_PORT
+    }
 });
 
 redisClient.on('error', (err) => console.error('Redis Client Error', err));
